6a9b9c18dd60136928b3366d5028c8fd72b2156c
Nemo Snel <nemo.snel2004@gmail.com>
2025-01-14 17:23:26 +0100

    exercise 3: relative precipitation

--- file1.py
+++ file1.py
@@ -22,13 +22,26 @@ for month in months:
         if key['date'] >= f'2010-{month}-01' and key['date'] <= f'2010-{month}-31':
             precipitation_month.append(key['value'])
             total_monthly_precipitation=total_monthly_precipitation + key['value']
-            total_precipitation_year=total_precipitation_year+ total_monthly_precipitation
-    print(f'{month}', total_monthly_precipitation)
-    results_precipitation[month]={'total_monthly_precipiation': total_monthly_precipitation}
+        results_precipitation[month]={'total_monthly_precipiation': total_monthly_precipitation}
 
 
-list_total_monthly_precipitation= {'{month}}': '{total_monthly_precipitation}'}
-print(list_total_monthly_precipitation)
+relative_precipitation_results={}
+total_precipitation_year=sum(precipitation_month)
+for month in months:
+    total_monthly_precipitation=0
+    for key in seattle:
+        if key['date'] >= f'2010-{month}-01' and key['date'] <= f'2010-{month}-31':
+            precipitation_month.append(key['value'])
+            total_monthly_precipitation=total_monthly_precipitation + key['value']
+        results_precipitation[month]={'total_monthly_precipiation': total_monthly_precipitation}
+        relative_precipitation=total_monthly_precipitation/total_precipitation_year
+    print('relative_precipitation', f'{month}',relative_precipitation)
+    relative_precipitation_results[month]={'monthly_relative_precipitation': relative_precipitation}
+
+results={'results_precipitation': results_precipitation, 'relative_precipitation_results': relative_precipitation_results}
+
+
+print('total_precipitation_year', total_precipitation_year)
+
 with open('results_precipitation.json', 'w')as file:
-    json.dump(results_precipitation, file, indent=4)
-print(total_precipitation_year)
+    json.dump(results, file, indent=4)
